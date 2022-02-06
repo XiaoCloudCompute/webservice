4cf90bb8daf242d9abe3452d70b1379ea6245360
xiaohuanlin <xiao.hua@northeastern.edu>
2022-02-06 14:54:42 -0500

    init code

---
+++ src/test/java/edu/xiao/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xiao.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
