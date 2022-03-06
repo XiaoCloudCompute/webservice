25f184788868e43dce773b1894ad245dfe1467b6
xiaohuanlin <33860497+xiaohuanlin@users.noreply.github.com>
2022-03-05 21:14:36 -0500

    remove parameter (#21)

--- src/main/java/edu/xiao/webservice/config/AmazonConfig.java
+++ src/main/java/edu/xiao/webservice/config/AmazonConfig.java
@@ -1,8 +1,5 @@
 package edu.xiao.webservice.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,23 +8,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
-    @Value("${aws.access_key}")
-    private String awsAccessKey;
-
-    @Value("${aws.secret_key}")
-    private String secretKey;
-
     @Value("${aws.region}")
     private String region;
 
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials =
-                new BasicAWSCredentials(awsAccessKey, secretKey);
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
